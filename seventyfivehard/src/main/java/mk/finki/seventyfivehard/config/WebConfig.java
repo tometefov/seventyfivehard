c1223a92ef974e80564eb8d4d6bad5b4e6edfd19
tometefov <tefov.tome@gmail.com>
2026-02-12 08:08:27 +0100

    added the k8s manifests

--- seventyfivehard/src/main/java/mk/finki/seventyfivehard/config/WebConfig.java
+++ seventyfivehard/src/main/java/mk/finki/seventyfivehard/config/WebConfig.java
@@ -5,6 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//public class WebConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:3000")
+//                        .allowedMethods("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//}
 @Configuration
 public class WebConfig {
 
@@ -14,7 +30,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
